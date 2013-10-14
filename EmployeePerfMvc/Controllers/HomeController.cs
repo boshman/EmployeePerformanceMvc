8ea7a20c3d4c9de1cb2c90c9365e85cd1aa019f5
boshman <BenjaminOshman@Maximus.com>
2013-10-13 22:57:33 -0400

    Modified to get data from database so EF would build out the database when I build the project.

--- EmployeePerfMvc/Controllers/HomeController.cs
+++ EmployeePerfMvc/Controllers/HomeController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EmployeePerfMvc.Models;
 
 namespace EmployeePerfMvc.Controllers
 {
     public class HomeController : Controller
     {
+
+        private EmployeePerfMvcDb _db = new EmployeePerfMvcDb();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+            var model = _db.Employees.ToList();
+
+            return View(model);
         }
 
         public ActionResult About()
