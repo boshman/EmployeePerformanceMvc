6a6b9a8ef7094ec4013984ed7d3c150e1110f4ed
boshman <BenjaminOshman@Maximus.com>
2013-10-13 22:55:39 -0400

    Added Employee and GoalAppraisal classes to project.

---
+++ EmployeePerfMvc/Models/EmployeePerfMvcDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EmployeePerfMvc.Models
+{
+    public class EmployeePerfMvcDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GoalAppraisal> GoalAppraisals { get; set; }
+    }
+}
---
+++ EmployeePerfMvc/Models/GoalAppraisal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeePerfMvc.Models
+{
+    public class GoalAppraisal
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int Year { get; set; }
+        public string Goal{ get; set; }
+        public string EmployeeAppraisal { get; set; }
+        public string ManagerAppraisal { get; set; }
+    }
+}
